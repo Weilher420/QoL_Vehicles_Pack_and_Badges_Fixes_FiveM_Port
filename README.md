046b7363c910fbbccb28451038e26adace2fee6b
Weilher <93231727+Weilher420@users.noreply.github.com>
2024-10-28 08:02:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fivem_port_quality_of_life_vehicles_pack_and_badges_fixes
+ A FiveM port of "Quality of Life Vehicle Fixes Pack" & "Badge Fixes Pack" mods made by austen64 
---
+++ quality_of_life_vehicle_pack/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+author "austen64"
+description "https://fr.gta5-mods.com/vehicles/quality-of-life-vehicle-pack-replace"
+version "1.14"
+
+files {
+	"meta/*.meta",
+	"xml/*.xml",
+}
+ 
+data_file "CARCOLS_FILE" "meta/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "meta/carvariations.meta"
